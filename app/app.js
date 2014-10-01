72b392903393f62f545323abe85b371ad955abcb
jlcanovas <me@jlcanovas.es>
2014-10-01 16:58:38 +0200

    Updating URLs

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
-var webpageURL = "http://localhost:8000/"
-var governanceURL = 'http://localhost:8080/fr.inria.atlanmod.governance.survey.web';
+var webpageURL = "http://atlanmod.github.io/governance"
+//var webpageURL = "http://localhost:8000"
+
+//var governanceURL = 'http://localhost:8080/fr.inria.atlanmod.governance.survey.web';
+var governanceURL = 'http://http://atlanmodexp.info.emn.fr:8800/governance';
 
 angular.module("governanceGenerator", 
 	[
